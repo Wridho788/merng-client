6edcb920766cf0a37b1be5b5122f478c55361b2b
ridhoDMI <105182858+ridhoDMI@users.noreply.github.com>
2022-06-22 22:28:49 +0700

    fix singlepost and revisi comment

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -15,6 +15,7 @@ import {
 import { AuthContext } from '../context/auth';
 import LikeButton from '../components/LikeButton';
 import DeleteButton from '../components/DeleteButton';
+import MyPopup from '../utils/MyPopup';
 
 function SinglePost(props, args = {}) {
   const postId = props.match.params.postId;
@@ -79,17 +80,19 @@ function SinglePost(props, args = {}) {
               <hr />
               <Card.Content extra>
                 <LikeButton user={user} post={{ id, likeCount, likes }} />
-                <Button
-                  as='div'
-                  labelPosition='right'
-                  onClick={() => console.log('Comment on post')}>
-                  <Button basic color='blue'>
-                    <Icon name='comments' />
+                <MyPopup content='Comment on post'>
+                  <Button
+                    as='div'
+                    labelPosition='right'
+                    onClick={() => console.log('Comment on post')}>
+                    <Button basic color='blue'>
+                      <Icon name='comments' />
+                    </Button>
+                    <Label basic color='blue' pointing='left'>
+                      {commentCount}
+                    </Label>
                   </Button>
-                  <Label basic color='blue' pointing='left'>
-                    {commentCount}
-                  </Label>
-                </Button>
+                </MyPopup>
                 {user && user.username === username && (
                   <DeleteButton postId={id} callback={deletePostCallback} />
                 )}
